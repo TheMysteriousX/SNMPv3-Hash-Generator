2d227225a842395bcc50582521ff20020dbb9a47
Adam Bishop <adam@omega.org.uk>
2020-07-12 18:18:44 +0100

    Run the project through some linters

--- generate_readme.rst.py
+++ generate_readme.rst.py
@@ -1,7 +1,6 @@
 import pypandoc
-import os
 
-output = pypandoc.convert_file('README.md', 'rst')
+output = pypandoc.convert_file("README.md", "rst")
 
-with open('README.rst','w+') as f:
+with open("README.rst", "w+") as f:
     f.write(output)
--- setup.py
+++ setup.py
@@ -1,44 +1,28 @@
 import os
 import sys
 
-if (sys.version_info < (3, 6)):
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 6):
     print("Python 3.6 or higher is required, please see https://www.python.org/ or your OS package repository", file=sys.stderr)
     sys.exit(2)
 
-from setuptools import setup, find_packages
+long_desc = open("README.md").read()
 
-long_desc=open('README.md').read()
-
-if os.path.exists('README.rst'):
-    long_desc = open('README.rst').read()
+if os.path.exists("README.rst"):
+    long_desc = open("README.rst").read()
 
 setup(
-    name='SNMPv3 Hash Generator',
-    version='0.3',
+    name="SNMPv3 Hash Generator",
+    version="0.3",
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'snmpv3-hashgen=scripts.snmpv3_hashgen:main',
-        ],
-    },
-
-    license='Apache Software License',
+    entry_points={"console_scripts": ["snmpv3-hashgen=scripts.snmpv3_hashgen:main"]},
+    license="Apache Software License",
     long_description=long_desc,
-
-    extras_require={
-        "YAML": ["PyYAML"],
-        "TOML": ["toml"],
-    },
-
+    extras_require={"YAML": ["PyYAML"], "TOML": ["toml"]},
     author="Adam Bishop",
     author_email="adam@omega.org.uk",
     description="Generates SNMPv3 hashes as described in rfc3414 suitable for use with ESXi and other SNMP daemons",
     url="https://github.com/TheMysteriousX/SNMPv3-Hash-Generator",
-
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.6',
-    ],
+    classifiers=["Development Status :: 4 - Beta", "License :: OSI Approved :: Apache Software License", "Programming Language :: Python :: 3.6"],
 )
-
