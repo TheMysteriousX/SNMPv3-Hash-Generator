678b124a87e06d72a9122473aa82b6bab87fa751
Adam Bishop <adam@omega.org.uk>
2020-07-12 19:06:27 +0100

    Create some tests and a tox environment

--- snmpv3_hashgen/hashgen.py
+++ snmpv3_hashgen/hashgen.py
@@ -46,8 +46,8 @@ class Hashgen(object):
 
 # Define available hash algorithms
 Hashgen.algs = {
-    "sha1": partial(Hashgen.hash, alg=hashlib.sha1),
     "md5": partial(Hashgen.hash, alg=hashlib.md5),
+    "sha1": partial(Hashgen.hash, alg=hashlib.sha1),
     "sha224": partial(Hashgen.hash, alg=hashlib.sha224),
     "sha256": partial(Hashgen.hash, alg=hashlib.sha256),
     "sha384": partial(Hashgen.hash, alg=hashlib.sha384),
