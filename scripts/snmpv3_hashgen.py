2a3f7d84ce000f200598afbcf9ec1a307048c7df
Adam Bishop <adam@omega.org.uk>
2020-07-12 18:18:44 +0100

    Run the project through some linters

--- scripts/snmpv3_hashgen.py
+++ scripts/snmpv3_hashgen.py
@@ -2,7 +2,7 @@
 
 import sys
 
-if (sys.version_info < (3, 6)):
+if sys.version_info < (3, 6):
     print("Python 3.6 or higher is required, please see https://www.python.org/ or your OS package repository", file=sys.stderr)
     sys.exit(2)
 
@@ -20,18 +20,19 @@ RFC 7630 defines no test data for sha[2-9]{3} - these should be considered exper
 Report bugs at https://github.com/TheMysteriousX/SNMPv3-Hash-Generator/issues
 """
 
-parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=help_text, epilog=epilog_text)
-parser.add_argument('--auth', type=str, help='Authentication passphrase to be derived as utf8 string')
-parser.add_argument('--priv', type=str, help='Privacy passphrase to be derived as utf8 string')
-parser.add_argument('--engine', type=str, help='Engine ID as hex string')
-parser.add_argument('--user', type=str, help='SNMPv3 USM username (default "librenms")')
-parser.add_argument('--mode', type=str, choices=['auth', 'priv', 'none'],  help='SNMPv3 mode (default "priv")')
-parser.add_argument('--hash', type=str, choices=['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'], help='Hash algorithm to use (default "sha1")', )
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=help_text, epilog=epilog_text,)
+parser.add_argument("--auth", type=str, help="Authentication passphrase to be derived as utf8 string")
+parser.add_argument("--priv", type=str, help="Privacy passphrase to be derived as utf8 string")
+parser.add_argument("--engine", type=str, help="Engine ID as hex string")
+parser.add_argument("--user", type=str, help='SNMPv3 USM username (default "librenms")')
+parser.add_argument("--mode", type=str, choices=["auth", "priv", "none"], help='SNMPv3 mode (default "priv")')
+parser.add_argument("--hash", type=str, choices=["md5", "sha1", "sha224", "sha256", "sha384", "sha512"], help='Hash algorithm to use (default "sha1")')
 
 fmt = parser.add_mutually_exclusive_group()
-fmt.add_argument('--json', action='store_true', help='Emit output as json', )
-fmt.add_argument('--yaml', action='store_true', help='Emit output as yaml')
-fmt.add_argument('--toml', action='store_true', help='Emit output as toml')
+fmt.add_argument("--json", action="store_true", help="Emit output as json")
+fmt.add_argument("--yaml", action="store_true", help="Emit output as yaml")
+fmt.add_argument("--toml", action="store_true", help="Emit output as toml")
+
 
 def format_esxi(user, Kul_auth, Kul_priv, mode, hash):
     if mode == "priv":
@@ -40,6 +41,7 @@ def format_esxi(user, Kul_auth, Kul_priv, mode, hash):
         return f"{user}/{hash(Kul_auth)}/-/{mode}"
     else:
         return f"{user}/-/-/{mode}"
+
 
 def main(*args, **kwargs):
     # Argument setup
@@ -54,29 +56,23 @@ def main(*args, **kwargs):
     auth = Hashgen.random_string() if not args.auth else args.auth
     priv = Hashgen.random_string() if not args.priv else args.priv
     engine = Hashgen.random_engine() if not args.engine else args.engine
-    hash = Hashgen.algs['sha1'] if not args.hash else Hashgen.algs[args.hash]
+    hash = Hashgen.algs["sha1"] if not args.hash else Hashgen.algs[args.hash]
 
-    # Derive Kul from passphrases
+    #  Derive Kul from passphrases
     try:
         Kul_auth = Hashgen.derive_msg(auth, engine, hash) if "none" not in mode else None
         Kul_priv = Hashgen.derive_msg(priv, engine, hash) if "priv" in mode else None
-    except ValueError as e:
+    except ValueError:
         print("Error: Engine ID seems invalid; ensure that it is entered as a hex character string", file=sys.stderr)
         sys.exit(1)
 
     esxi = format_esxi(user, Kul_auth, Kul_priv, mode, hash)
     output = {
-            'user': user,
-            'engine': engine,
-            'phrases': {
-                'auth': auth if "none" not in mode else None,
-                'priv': priv if "priv" in mode else None,
-            },
-            'hashes': {
-                'auth': hash(Kul_auth) if "auth" in mode or "priv" in mode else None,
-                'priv': hash(Kul_priv) if "priv" in mode else None,
-            },
-            'esxi': esxi,
+        "user": user,
+        "engine": engine,
+        "phrases": {"auth": auth if "none" not in mode else None, "priv": priv if "priv" in mode else None},
+        "hashes": {"auth": hash(Kul_auth) if "auth" in mode or "priv" in mode else None, "priv": hash(Kul_priv) if "priv" in mode else None},
+        "esxi": esxi,
     }
 
     if args.json:
@@ -84,18 +80,20 @@ def main(*args, **kwargs):
     elif args.yaml:
         try:
             import yaml
-            print(yaml.dump({'snmpv3': output}, explicit_start=True))
+
+            print(yaml.dump({"snmpv3": output}, explicit_start=True))
         except ImportError:
-            print('YAML output requires a YAML library')
-            print('Try running pip3 install PyYaml')
+            print("YAML output requires a YAML library")
+            print("Try running pip3 install PyYaml")
             sys.exit(4)
     elif args.toml:
         try:
             import toml
+
             print(toml.dumps(output))
         except ImportError:
-            print('TOML output requires a TOML library')
-            print('Try running pip3 install toml')
+            print("TOML output requires a TOML library")
+            print("Try running pip3 install toml")
             sys.exit(5)
     else:
         print(f"User: {user}")
@@ -105,6 +103,7 @@ def main(*args, **kwargs):
             print(f"Priv: {priv} / {hash(Kul_priv)}")
         print(f"Engine: {engine}")
         print(f"ESXi USM String: {esxi}")
+
 
 if __name__ == "__main__":
     main()
--- snmpv3_hashgen/hashgen.py
+++ snmpv3_hashgen/hashgen.py
@@ -8,6 +8,7 @@ from functools import partial
 P_LEN = 32
 E_LEN = 16
 
+
 class Hashgen(object):
     @staticmethod
     def hash(bytes, alg=hashlib.sha1, raw=False):
@@ -15,20 +16,20 @@ class Hashgen(object):
         return digest if raw else digest.hex()
 
     @staticmethod
-    def expand(s, l):
-        reps = l // len(s) + 1 # approximation; worst case: overrun = l + len(s)
-        return ''.join(list(repeat(s, reps)))[:l]
+    def expand(substr, target_len):
+        reps = target_len // len(substr) + 1  # approximation; worst case: overrun = l + len(s)
+        return "".join(list(repeat(substr, reps)))[:target_len]
 
     @staticmethod
     def kdf(password, alg=None):
-        alg = Hashgen.algs['sha1'] if alg is None else alg
+        alg = Hashgen.algs["sha1"] if alg is None else alg
 
-        data = Hashgen.expand(password, 1048576).encode('utf-8')
+        data = Hashgen.expand(password, 1048576).encode("utf-8")
         return alg(data, raw=True)
 
     @staticmethod
     def random_string(len=P_LEN, alphabet=(string.ascii_letters + string.digits)):
-        return ''.join(secrets.choice(alphabet) for _ in range(len))
+        return "".join(secrets.choice(alphabet) for _ in range(len))
 
     @staticmethod
     def random_engine(len=E_LEN):
@@ -40,14 +41,15 @@ class Hashgen(object):
         Ku = Hashgen.kdf(passphrase, alg)
         E = bytearray.fromhex(engine)
 
-        return b''.join([Ku, E, Ku])
+        return b"".join([Ku, E, Ku])
+
 
 # Define available hash algorithms
 Hashgen.algs = {
-    'sha1':   partial(Hashgen.hash, alg=hashlib.sha1),
-    'md5':    partial(Hashgen.hash, alg=hashlib.md5),
-    'sha224': partial(Hashgen.hash, alg=hashlib.sha224),
-    'sha256': partial(Hashgen.hash, alg=hashlib.sha256),
-    'sha384': partial(Hashgen.hash, alg=hashlib.sha384),
-    'sha512': partial(Hashgen.hash, alg=hashlib.sha512),
+    "sha1": partial(Hashgen.hash, alg=hashlib.sha1),
+    "md5": partial(Hashgen.hash, alg=hashlib.md5),
+    "sha224": partial(Hashgen.hash, alg=hashlib.sha224),
+    "sha256": partial(Hashgen.hash, alg=hashlib.sha256),
+    "sha384": partial(Hashgen.hash, alg=hashlib.sha384),
+    "sha512": partial(Hashgen.hash, alg=hashlib.sha512),
 }
