30df67e7da43651045da1cb3d323ba132298f5f4
Adam Bishop <adam@omega.org.uk>
2020-07-12 20:54:14 +0100

    Change python version restriction technique

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@ import sys
 
 from setuptools import setup, find_packages
 
-if sys.version_info < (3, 6):
-    print("Python 3.6 or higher is required, please see https://www.python.org/ or your OS package repository", file=sys.stderr)
-    sys.exit(2)
-
 long_desc = open("README.md").read()
 
 if os.path.exists("README.rst"):
@@ -25,4 +21,5 @@ setup(
     description="Generates SNMPv3 hashes as described in rfc3414 suitable for use with ESXi and other SNMP daemons",
     url="https://github.com/TheMysteriousX/SNMPv3-Hash-Generator",
     classifiers=["Development Status :: 4 - Beta", "License :: OSI Approved :: Apache Software License", "Programming Language :: Python :: 3.6"],
+    python_requires='>=3.6',
 )
